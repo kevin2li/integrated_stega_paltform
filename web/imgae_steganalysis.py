7173cbbc308a09a212705f24f43d0ee6f1cf2473
kevin2li <kevin2li@qq.com>
2021-04-23 20:02:24 +0800

    update yednet and add image visulization

--- web/imgae_steganalysis.py
+++ web/imgae_steganalysis.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-04-22 11:08:05
-LastEditTime: 2021-04-23 18:43:15
+LastEditTime: 2021-04-23 20:20:26
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /myapps/web/imgae_steganalysis.py
@@ -9,12 +9,17 @@ FilePath: /myapps/web/imgae_steganalysis.py
 import os
 import sys
 sys.path.append(os.path.abspath('..'))
+
+import pandas as pd
+
 import torch
-from h2o_wave import Q, app, handle_on, main, on, ui
-from icecream import ic
-from web.utils import *
 import torch.nn.functional as F
+from h2o_wave import Q, app, handle_on, main, on, ui, data
+from icecream import ic
 from project.sa import YedNet, ZhuNet
+
+from web.utils import *
+
 models = {'YedNet': YedNet, 'ZhuNet': ZhuNet}
 #================================================================
 # 图像隐写分析
@@ -24,15 +29,12 @@ async def image_instance_level(q:Q):
     q.page['meta'] = layout2
     q.page['content_left'] = ui.form_card(box=ui.box('content_left', order=2, height='550px'), title='Inputs', items=[
         ui.text('**上传可疑图片:**'),
-        ui.file_upload(name='suspect_img', label='上传', multiple=False, file_extensions=['png', 'jpg', 'jpeg'], max_file_size=10, max_size=15, height='3'),
-        # ui.dropdown('option', label='隐写分析模型:', value='SRNet', choices=[
-        #     ui.choice('SRNet', 'SRNet'),
-        #     ui.choice('ZhuNet', 'ZhuNet'),
-        #     ui.choice('YedNet', 'Yedroudj-Net'),
-        #     ui.choice('srm', 'SRM'),
-        # ]),
-        ui.checklist(name='checklist', label='隐写分析模型',
-                        choices=[ui.choice(name=x, label=x) for x in ['SRNet', 'ZhuNet', 'YedNet', 'SRM']]),
+        ui.file_upload(name='suspect_img', label='上传', multiple=False, file_extensions=['png', 'jpg', 'jpeg'], max_file_size=10, max_size=15, height='200px'),
+        ui.dropdown('options', label='隐写分析模型:', values=['SRNet'], required=True, choices=[
+            ui.choice(name=x, label=x) for x in ['SRNet', 'ZhuNet', 'YedNet', 'XuNet', 'SRM', 'A', 'B', 'C', 'D', 'E', 'F']
+        ]),
+        # ui.checklist(name='checklist', label='隐写分析模型',
+        #                 choices=[ui.choice(name=x, label=x) for x in ['SRNet', 'ZhuNet', 'YedNet', 'XuNet', 'SRM', 'A', 'B', 'C', 'D', 'E', 'F']]),
 
         ui.button('image_start_analysis', label='开始检测', primary=True)
     ])
@@ -45,8 +47,8 @@ async def image_instance_level(q:Q):
 @on()
 async def image_dataset_level(q:Q):
     q.page['content_left'] = ui.form_card(box=ui.box('content_left', order=2, height='550px'), title='Inputs', items=[
-        ui.checklist(name='checklist', label='数据集', choices=[ui.choice(name=x, label=x) for x in ['S-UNIWARD 0.4bpp', 'WOW 0.4bpp', 'HUGO 0.4bpp']]),
-        ui.checklist(name='checklist', label='隐写分析模型', choices=[ui.choice(name=x, label=x) for x in ['SRNet', 'ZhuNet', 'YedNet', 'SRM']]),
+        ui.dropdown(name='options2', label='数据集', required=True, values=['S-UNIWARD 0.4bpp'], choices=[ui.choice(name=x, label=x) for x in ['S-UNIWARD 0.4bpp', 'WOW 0.4bpp', 'HUGO 0.4bpp']]),
+        ui.dropdown(name='options3', label='隐写分析模型', required=True, values=['SRNet'], choices=[ui.choice(name=x, label=x) for x in ['SRNet', 'ZhuNet', 'YedNet', 'SRM']]),
         ui.button('start_dataset_analysis', label='开始检测', primary=True)
     ])
     await q.page.save()
@@ -65,12 +67,12 @@ async def suspect_img(q:Q):
 @on()
 async def image_start_analysis(q:Q):
     suspect_img_path = q.client.suspect_img_path
-    checklist = q.args['checklist']
+    options = q.args['options']
     ic(suspect_img_path)
-    ic(checklist)
-    if suspect_img_path and checklist:
+    ic(options)
+    if suspect_img_path and options:
         FLAG = False
-        for i in checklist:
+        for i in options:
             if i not in ('ZhuNet', 'YedNet'):
                 FLAG = True
                 break
@@ -83,7 +85,7 @@ async def image_start_analysis(q:Q):
             img = img_preprocess(suspect_img_path)
             ic(img.shape)
             result = {}
-            for i in checklist:
+            for i in options:
                 model = models[i]()
                 if i == 'ZhuNet':
                     params = torch.load('/home/kevin2li/wave/myapps/project/sa/zhunet/zhunet_wow.ptparams', map_location='cpu')
@@ -92,10 +94,12 @@ async def image_start_analysis(q:Q):
                 out = F.softmax(out, dim=-1).squeeze()
                 result[i] = out.tolist()
             ic(result)
-            
+            df = pd.DataFrame()
+            df['type'] = ['cover', 'stego']
+            df['prob'] = result['ZhuNet']
             q.page['content_right'] = ui.form_card(box=ui.box('content_right'), title='Outputs', items=[
-                ui.text(f'检测模型:{checklist[0]}'),
                 ui.text(f"结果:{str(result)}"),
+                ui.visualization(ui.plot([ui.mark(type='interval', x='=type', y='=prob', x_title='类型', y_title='概率')]), data=data(fields=df.columns.tolist(), rows=df.values.tolist(), pack=True))
             ])
             q.page['content_left'].items[1].file_upload.label = '上传'
             # del q.client.suspect_img_path
