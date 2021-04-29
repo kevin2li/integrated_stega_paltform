38714f7bd3d599dd6d61ace19f0e9bfc0fa35d2a
kevin2li <kevin2li@qq.com>
2021-04-29 10:28:16 +0800

    update

---
+++ project/__init__.py
@@ -0,0 +1,6 @@
+
+from pathlib import Path
+
+# root_dir = Path('/home/kevin2li/wave/myapps/')  # wsl
+root_dir = Path('/root/wave/myapp')  # aliyun
+# root_dir = Path('/home/likai/integrated_stega_paltform/') # lab
