67456bd4ab569d5093e00b0234bce305ebc382c9
kevin2li <kevin2li@qq.com>
2021-05-27 20:02:31 +0800

    support mutiple model

--- web/imgae_steganalysis.py
+++ web/imgae_steganalysis.py
@@ -21,8 +21,8 @@ import torch.nn.functional as F
 from h2o_wave import Q, app, data, handle_on, main, on, ui
 from icecream import ic
 from project import root_dir
-from project.steganalysis.tf.models import XuNet
-from project.steganalysis.torch.models import YedNet, ZhuNet
+from project.steganalysis.torch.models import YedNet, ZhuNet, SRNet, YeNet, XuNet
+from project.steganalysis.tf.models import XuNet as XuNet_tf
 
 from web.utils import *
 
@@ -41,28 +41,18 @@ async def image_instance_level(q:Q):
             ), title='Inputs', items=[
         ui.text('**1. 上传可疑图片:**'),
         ui.file_upload(name='suspect_img', label='上传', multiple=False, file_extensions=['png', 'jpg', 'jpeg'], max_file_size=10, max_size=15, height='200px'),
-        # ui.expander(name='expander', label='高级选项', items=[
-        #     ui.dropdown('framework', label='框架:', value='pytorch', required=True, choices=[
-        #         ui.choice(name=x, label=x) for x in ['pytorch', 'tensorflow']
-        #     ]),
-        #     ui.dropdown('source', label='数据集:', value='WOW', required=True, choices=[
-        #         ui.choice(name=x, label=x) for x in ['WOW', 'S-UNIWARD', 'HILL', 'HUGO', 'MG', 'MVG', 'UT-GAN', 'MiPOD']
-        #     ]),
-        #     ui.dropdown('embedding_rate', label='嵌入率:', value='0.4 bpp', required=True, choices=[
-        #         ui.choice(name=x, label=x) for x in ['0.2 bpp', '0.4 bpp', '0.6 bpp', '0.8 bpp']
-        #     ]),
-        # ]),
+
         ui.text('**2. 选择模型:**'),
-        ui.dropdown('framework', label='框架:', value='pytorch', required=True, choices=[
+        ui.dropdown('framework', label='框架(单选):', value='pytorch', required=True, choices=[
             ui.choice(name=x, label=x) for x in ['pytorch', 'tensorflow']
         ]),
-        ui.dropdown('dataset', label='数据集:', value='WOW', required=True, choices=[
+        ui.dropdown('source', label='数据集(单选):', value='WOW', required=True, choices=[
             ui.choice(name=x, label=x) for x in ['WOW', 'S-UNIWARD', 'HILL', 'HUGO', 'MG', 'MVG', 'UT-GAN', 'MiPOD']
         ]),
-        ui.dropdown('embedding_rate', label='嵌入率:', value='0.4 bpp', required=True, choices=[
+        ui.dropdown('embedding_rate', label='嵌入率(单选):', value='0.4 bpp', required=True, choices=[
             ui.choice(name=x, label=x) for x in ['0.2 bpp', '0.4 bpp', '0.6 bpp', '0.8 bpp']
         ]),
-        ui.dropdown('models', label='隐写分析模型:', values=['SRNet'], required=True, choices=[
+        ui.dropdown('options', label='隐写分析模型(可多选):', values=['SRNet'], required=True, choices=[
             ui.choice(name=x, label=x) for x in ['SRNet', 'ZhuNet', 'YedNet', 'XuNet', 'YeNet']
         ]),
 
@@ -95,34 +85,30 @@ async def suspect_img(q:Q):
         ic(local_path)
         q.page['content_left'].items[1].file_upload.label = '已上传'
         # q.page['content_left'].items[6].button.disabled = False
-        q.client.upload = True
     await q.page.save()
 
 @on()
 async def image_start_analysis(q:Q):
-    ic('erhsjfksdf')
     # get inputs
     suspect_img_path = q.client.suspect_img_path
-    models = q.args['models']
+    models = q.args['options']
     framework = q.args['framework']
-    dataset = q.args['dataset']
+    dataset = q.args['source']
     embedding_rate = q.args['embedding_rate']
-    ic(models)
     # keep state
-    q.page['content_left'].items[3].dropdown.values=[framework]
-    q.page['content_left'].items[4].dropdown.values=[dataset]
-    q.page['content_left'].items[5].dropdown.values=[embedding_rate]
+    q.page['content_left'].items[3].dropdown.value=framework
+    q.page['content_left'].items[4].dropdown.value=dataset
+    q.page['content_left'].items[5].dropdown.value=embedding_rate
     q.page['content_left'].items[6].dropdown.values=models
 
     ic(suspect_img_path)
-    if not q.client.upload:
+    if not suspect_img_path:
         q.page['meta'].dialog = ui.dialog(title='error', items=[
             ui.text('对不起，请先上传图片!'),
             ui.buttons([ui.button(name='sure', label='确定', primary=True)])
         ])
     if suspect_img_path and models and framework and dataset and embedding_rate:
-        ic('ok')
-        def predict(suspect_img_path, models):
+        def predict(suspect_img_path, models, embedding_rate, framework, dataset):
             result = {}
             for model_name in models:
                 # read map.yml
@@ -130,28 +116,31 @@ async def image_start_analysis(q:Q):
                 with open(path) as f:
                     args = yaml.safe_load(f)
                 # get checkpoint_path
-                embedding_rate = embedding_rate.split()[0]
+                embedding_rate = 0.4
                 checkpoint_path = args['framework'][framework]['dataset'][dataset]['embedding_rate'][embedding_rate]['model'][model_name]
                 ic(checkpoint_path)
-                img = img_preprocess(suspect_img_path)
                 # load model and weights
-                model = eval(model_name)()
-                if framework == 'torch':
+                out = None
+                if framework == 'pytorch':
+                    model = eval(model_name)()
                     if checkpoint_path:
                         model = model.load_from_checkpoint(checkpoint_path)
                     model.eval()
+                    img = img_preprocess(suspect_img_path)
                     out = model(img)
                     out = F.softmax(out, dim=-1).squeeze()
                 elif framework == 'tensorflow':
-                    img = np.array(Image.open(suspect_img_path))
+                    model = eval(f"{model_name}_tf")()
                     if checkpoint_path:
                         model.load_weights(checkpoint_path)
+                    img = np.array(Image.open(suspect_img_path))
                     out = model(img).squeeze()
+                ic(out)
                 result[model_name] = out.tolist()
             return result
         q.page['content_right'] = ui.form_card(box='content_right', title='Outputs', items=[ui.progress('检测中...')])
         await q.page.save()
-        result = await q.run(predict, suspect_img_path, models)
+        result = await q.run(predict, suspect_img_path, models, embedding_rate, framework, dataset)
         ic(result)
         # df = pd.DataFrame(result)
         # df['type'] = ['cover', 'stego']
@@ -176,5 +165,5 @@ async def image_start_analysis(q:Q):
             content=f"**image:** {suspect_img_path.split('/')[-1]}\n\n ![plot]({suspect_img_path2})\n\n **result:**\n\n ![plot]({result_img_path})"
         )
         q.page['content_left'].items[1].file_upload.label = '上传'
-
+        q.client.suspect_img_path = None
     await q.page.save()
